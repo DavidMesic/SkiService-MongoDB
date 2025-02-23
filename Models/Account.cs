5b5a4589725482b086cd02e6d7f9658b79192578
DavidMesic <142584788+DavidMesic@users.noreply.github.com>
2025-02-23 16:29:58 +0100

    Account.cs model customized for MongoDB

--- Models/Account.cs
+++ Models/Account.cs
@@ -1,31 +1,34 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
 
 public class Account
 {
-    [Key]
-    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-    public int AccountID { get; set; }
+    [BsonId]
+    [BsonElement("_id"), BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+    public string? AccountID { get; set; }
 
     [Required]
     [MaxLength(50)]
+    [BsonElement("username"), BsonRepresentation(MongoDB.Bson.BsonType.String)]
     public string Benutzername { get; set; }
 
     [Required]
-    public string PasswortHash { get; set; }
-
-    [Required]
     [MaxLength(100)]
+    [BsonElement("email"), BsonRepresentation(MongoDB.Bson.BsonType.String)]
     public string Email { get; set; }
 
+    [Required]
+    [BsonElement("password"), BsonRepresentation(MongoDB.Bson.BsonType.String)]
+    public string PasswortHash { get; set; }
+
     [MaxLength(20)]
+    [BsonElement("phone"), BsonRepresentation(MongoDB.Bson.BsonType.String)]
     public string Telefon { get; set; } = null;
 
     [MaxLength(20)]
     [JsonIgnore]
+    [BsonElement("role"), BsonRepresentation(MongoDB.Bson.BsonType.String)]
     public string Rolle { get; set; } = "Kunde";
-
-    [JsonIgnore]
-    public ICollection<Auftrag>? Aufträge { get; set; }
 }
